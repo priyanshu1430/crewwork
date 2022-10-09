a536df489d41dcd720d162cfb3a2ac5a1bbe6dc7
priyanshu1430 <85101264+priyanshu1430@users.noreply.github.com>
2022-10-09 22:30:36 +0530

    Add files via upload

---
+++ month3/week2/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "362e3ff551msh885a787127e4e22p121fffjsn93046416d14a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7a064ae8dd325db918454971ac79068b"; 
